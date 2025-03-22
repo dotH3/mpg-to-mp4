83e1865e9caf517b8a54647f25f8939d66b0fe67
dotH3 <joaquinbastias321@gmail.com>
2025-03-22 16:44:45 -0300

    for each y mutacion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+
 fn main() {
-    println!("Hello, world!!");
+    // Comentario en Rust
+    let mut x: &str = "Hola Mundo!";
+    println!("{}",x);
+    x = "Hello World!";
+    println!("{}",x);
+
+    let path = "./input";
+    let z = std::fs::read_dir(path).unwrap();
+    
+    for n in z {
+        let n = n.unwrap();
+        let file_name = n.file_name(); 
+        let file_type = n.file_type().unwrap().is_dir();
+        println!("{:?} {:?}", file_name, file_type);
+    }
 }

1cc5134eab5ba62bb0cd48ae8b707b8a8efbff7c
dotH3 <joaquinbastias321@gmail.com>
2025-03-24 23:29:28 -0300

    Refactor file processing in main function to count and display valid .mpg files

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 
 fn main() {
-    // Comentario en Rust
-    let mut x: &str = "Hola Mundo!";
-    println!("{}",x);
-    x = "Hello World!";
-    println!("{}",x);
-
     let path = "./input";
     let z = std::fs::read_dir(path).unwrap();
+
+    let mut file_counter_success = 0;
     
     for n in z {
         let n = n.unwrap();
         let file_name = n.file_name(); 
-        let file_type = n.file_type().unwrap().is_dir();
-        println!("{:?} {:?}", file_name, file_type);
+        let is_file = n.file_type().unwrap().is_file();
+        let is_mpg = file_name.to_str().unwrap().ends_with(".mpg");
+        if is_file == false || is_mpg == false {
+            continue;
+        };
+
+        // convertir a mp4
+        let buffer = std::fs::read(n.path()).unwrap();
+        let buffer = buffer.len() as f64 / 1024.0 / 1024.0;
+        println!("buffer size: {:.2}MB",buffer);
+        
+
+        println!("{:?} {:?}", file_name, n.path());
+        file_counter_success += 1 ;
     }
+
+    println!("Total de archivos mpg: {}", file_counter_success);
 }
